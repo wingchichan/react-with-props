c1e03877e9ac4c1792d3c18e437fb9e77b5c45fc
wingchichan <w.chi.chan@gmail.com>
2025-02-11 11:16:56 +0000

    react with props

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Painting from "./components/Painting";
+export default function App() {
+  return (
+    <>
+      <Painting
+        painting="https://cdn.shopify.com/s/files/1/1159/6308/products/TropicalBliss_product_480x480.jpg?v=1632224885"
+        alt_text="alt text"
+      />
+    </>
+  );
+}
---
+++ src/components/Painting.jsx
@@ -0,0 +1,3 @@
+export default function Painting({ painting, alt_text }) {
+  return <img src={painting} alt={alt_text} />;
+}
